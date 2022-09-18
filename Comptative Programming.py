6611f952f141cab6740873ab0312cc51631fc63b
gruhasatya <66037927+gruhasatya@users.noreply.github.com>
2022-09-18 16:22:54 +0530

    commit

--- Comptative Programming.py
+++ Comptative Programming.py
@@ -89,3 +89,29 @@ def KnightTourMain():                                                           
 KnightTourMain()
 
 # The Knight’s tour problem is a famous problem in computer science and mathematics.
+
+
+
+# Basic - 1
+# Discuss tower of hanoi puzzle
+
+def tower_of_hanoi(n, source, destination, auxiliary):
+    if n == 1:
+        print("Move disk 1 from source", source, "to destination", destination)
+        return
+    tower_of_hanoi(n - 1, source, auxiliary, destination)
+    print("Move disk", n, "from source", source, "to destination", destination)
+    tower_of_hanoi(n - 1, auxiliary, destination, source)
+
+n = 4
+tower_of_hanoi(n, 'A', 'B', 'C')
+
+# The Tower of Hanoi is a mathematical puzzle where we have three rods and n disks.
+# The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+# 1. Only one disk can be moved at a time.
+# 2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e.
+# a disk can only be moved if it is the uppermost disk on a stack.
+# 3. No disk may be placed on top of a smaller disk.
+
+# Basic - 2
+# Discuss N-Queen problem
